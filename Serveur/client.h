9827e98047c04b5584bf3b975ea5680ea6ad8b7b
isabellebryans <isabellebryans@gmail.com>
2022-12-01 18:43:44 +0100

    get group name and members functions working, need to fill in the create group function groupchat struct defined

--- Serveur/client.h
+++ Serveur/client.h
@@ -8,5 +8,12 @@ typedef struct
    SOCKET sock;
    char name[BUF_SIZE];
 }Client;
+typedef struct
+{
+   char name[BUF_SIZE];
+   int size;
+   Client members[MAX_NAME];
+}Groupchat;
+
 
 #endif /* guard */
